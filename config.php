58db85923a0f898cb1e2a598234ea7f457133bea
Hybernia <113475061+hybernia1@users.noreply.github.com>
2026-04-02 03:59:01 +0200

    Load DB config constants from root config file

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+define('INC_DIR', 'inc/');
+define('CLASS_DIR', INC_DIR . 'Class');
+
+const DB_HOST = 'db.dw173.webglobe.com';
+const DB_NAME = 'iluze_net6';
+const DB_USER = 'iluze_net6';
+const DB_PASS = 'p27uuejD';
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 declare(strict_types=1);
 
+require_once __DIR__ . '/config.php';
 require_once __DIR__ . '/inc/Class/Db/Connection.php';
 require_once __DIR__ . '/inc/Class/Db/Query.php';
 require_once __DIR__ . '/inc/Class/Auth/Auth.php';
