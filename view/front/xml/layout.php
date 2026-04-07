862e5a6418ad8bf82a5d1a6efb6eaede04b68d5d
Hybernia <113475061+hybernia1@users.noreply.github.com>
2026-04-07 16:06:08 +0200

    Fix XML response Content-Type header

--- view/front/xml/layout.php
+++ view/front/xml/layout.php
@@ -1,6 +1,7 @@
 <?php
 declare(strict_types=1);
-header((string)($contentType ?? 'application/xml; charset=utf-8'));
+$type = trim((string)($contentType ?? 'application/xml; charset=utf-8'));
+header('Content-Type: ' . ($type !== '' ? $type : 'application/xml; charset=utf-8'));
 echo '<?xml version="1.0" encoding="UTF-8"?>';
 ?>
 <?= $content ?>
