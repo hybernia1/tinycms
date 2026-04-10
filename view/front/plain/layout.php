2afae7d24117ca913c30aa6bebe970af32618e1b
Hybernia <113475061+hybernia1@users.noreply.github.com>
2026-04-10 08:54:23 +0200

    Refactor 404 image response to use plain layout

--- view/front/plain/layout.php
+++ view/front/plain/layout.php
@@ -1,5 +1,6 @@
 <?php
 declare(strict_types=1);
-header('Content-Type: text/plain; charset=utf-8');
+$type = trim((string)($contentType ?? 'text/plain; charset=utf-8'));
+header('Content-Type: ' . ($type !== '' ? $type : 'text/plain; charset=utf-8'));
 ?>
 <?= $content ?>
--- view/front/svg/layout.php
+++
@@ -1,5 +0,0 @@
-<?php
-declare(strict_types=1);
-header('Content-Type: image/svg+xml; charset=utf-8');
-?>
-<?= $content ?>
