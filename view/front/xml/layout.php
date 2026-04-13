b623d064638517a0e8c8f8c179b41b8cb897146e
Hybernia <113475061+hybernia1@users.noreply.github.com>
2026-04-13 11:48:40 +0200

    chore: remove unused plain/xml front layouts

--- view/front/xml/layout.php
+++
@@ -1,7 +0,0 @@
-<?php
-declare(strict_types=1);
-$type = trim((string)($contentType ?? 'application/xml; charset=utf-8'));
-header('Content-Type: ' . ($type !== '' ? $type : 'application/xml; charset=utf-8'));
-echo '<?xml version="1.0" encoding="UTF-8"?>';
-?>
-<?= $content ?>
